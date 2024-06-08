360297c988a25c05ece2c25f7d515c2718a9b4db
Snehal <snehal.cs21@bmsce.ac.in>
2024-06-09 04:52:53 +0530

    added

---
+++ index.js
@@ -0,0 +1,43 @@
+// const express = require('express')
+// const app = express()
+// require('dotenv').config()
+// app.get('/home', (req,res)=>{
+//    res.send("this is first ")
+// })
+// app.get('/login', (req,res)=>{
+//     res.send('<h1>LOGIN WOULD TAKE TIME</h1>')
+// })
+// app.listen(process.env.PORT, ()=>{
+//     console.log("server has started");
+// })
+// deploy it in production
+
+// const express = require('express')
+// const app = express()
+// const cors = require('cors')
+// app.use(cors ())  
+// const joke = [
+//     {
+//         id: 1,
+//         title: "joke 1",
+//         content: "this is joke 1"
+//     },
+//     {
+//         id: 2,
+//         title: "joke 2",
+//         content: "this is joke 2"
+//     },
+//     {
+//         id: 3,
+//         title: "joke 3",
+//         content: "this is joke 3"
+//     }
+// ]
+// app.get('/api/jokes', (req, res)=>{
+ 
+//     res.send(joke);
+// })
+// app.listen(3000, ()=>{
+//     console.log("server has started ")
+// })
+
